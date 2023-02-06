8042492bc969f874665e8ed7d609959aa6dca1ea
lin <unknopu@gmail.com>
2023-02-06 17:33:16 +0700

    update

--- main.go
+++ main.go
@@ -19,10 +19,6 @@ func main() {
 		return c.HTML(http.StatusOK, "Hello, Docker! <3\n")
 	})
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
 		httpPort = "1323"
